8cd54df9ccc6224520760b591b7ac98db3812e22
Lana Born <57254972+Lana110370@users.noreply.github.com>
2021-12-15 10:52:35 +0000

    Evaluatiemethode van MLAgent toegevoegd

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from _core import is_winner, opponent, start
 from _agent import EvaluationAgent
 import random
 
-class MyRandomAgent(EvaluationAgent):
-    def evaluate(self, board, my_symbol, opponent_symbol):
-        return random.randint(1, 500)
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
     
-    
-my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent)
+ 
+my_agent = MyAgent()
+start(player_o=my_agent)
