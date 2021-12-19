55fcb816a1677f7438f533dffb0a7dd0c1511381
Lana Born <57254972+Lana110370@users.noreply.github.com>
2021-12-19 20:11:28 +0000

    Er wordt een lijngrafiek geplot

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ class MyAgent(MLAgent):
             reward = 0
         return reward
      
+random.seed(1)
+ 
 my_agent = MyAgent()
-
-my_agent = load('MyAgent_3000')
+random_agent = RandomAgent()
  
-my_agent.learning = False
- 
-validation_agent = RandomAgent()
- 
-validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
-
-plot_validation(validation_result)
+train_and_plot(
+    agent=my_agent,
+    validation_agent=random_agent,
+    iterations=50,
+    trainings=100,
+    validations=1000)
